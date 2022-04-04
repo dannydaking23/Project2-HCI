a93f76fc00acc4e3847a074dbe9bab82ead0b144
dannydaking23 <danielking2019@gmail.com>
2022-04-04 14:28:51 -0400

    Sidebar Update

--- Project2-HCI.py
+++ Project2-HCI.py
@@ -2,12 +2,35 @@ import streamlit as st
 import requests as rq
 import numpy as np
 
-pageTitle = "TEMP"
 st.set_page_config(
-    page_title=pageTitle,
+    page_title = "HCI - Project 2",
+    layout = "wide",
     menu_items={
-        "About": "HCI Project 2"
+        "Get Help" : "https://docs.streamlit.io/",
+        "About" : "Welcome to Project Bulldozer's Weather App"
     }
 )
 
-st.title(pageTitle)
+st.title("Weather App")
+st.header("HCI - Project Bulldozer")
+
+add_selectbox = st.sidebar.selectbox(
+    "Select a Feature",
+    ["Homepage", "Weather Report", "Global Warming", "Natural Disasters"]
+)
+
+if add_selectbox == "Weather Report":
+    st.subheader("Weather Report")
+
+
+elif add_selectbox == "Global Warming":
+    st.subheader("Global Warming")
+
+
+elif add_selectbox == "Natural Disasters":
+    st.subheader("Natural Disasters")
+
+
+else:
+    st.subheader("Who We Are")
+
