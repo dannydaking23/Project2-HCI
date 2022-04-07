6635b5aa9533505cafa35a7824b2024a7a13ea60
dannydaking23 <danielking2019@gmail.com>
2022-04-07 02:00:58 -0400

    DC's Update along with Map and Time Dataframe

--- Project2-HCI.py
+++ Project2-HCI.py
@@ -35,185 +35,147 @@ def map_creator(latitude,longitude):
     folium_static(m)
 
 # streamlit structure below
-pageTitle = "Weather"
+pageTitle = "Current Weather Info"
 st.set_page_config(
     page_title=pageTitle,
+    layout = "wide",
     menu_items={
         "Get Help" : "https://docs.streamlit.io/",
         "About": "HCI Project 2"
     }
 )
 
-st.title(pageTitle)
-# lat lon from ip
-g = geocoder.ipinfo('me')
-lat = g.latlng[0]
-lon = g.latlng[1]
-city = g.city
+c1, c2 = st.columns([2, 1])
 
-st.info("Based on your IP address, you are in or near " + city + ".")
-citySearchInput = st.text_input("Search for a city:")
-if citySearchInput:
-    citySearchURL = "https://nominatim.openstreetmap.org/search?city=" + citySearchInput + "&format=json"
-    citySearchResponse = rq.get(citySearchURL).json()
-    cityOptions = []
-    for item in citySearchResponse:
-        if item["display_name"] not in cityOptions:
-            cityOptions.append(item["display_name"])
-    # cityOptions = list(set(cityOptions))
-    selectedCity = st.selectbox(str(len(cityOptions)) + " results for " + citySearchInput, cityOptions)
-    selectedLat = 0.
-    selectedLon = 0.
-    for item in citySearchResponse:
-        if item["display_name"] == selectedCity:
-            selectedLat = item["lat"]
-            selectedLon = item["lon"]
-            break
-    city = selectedCity
-    lat = selectedLat
-    lon = selectedLon
+response = {}
 
-cityShortName = ""
-for i in range(len(city)):
-    if city[i] != ',':
-        cityShortName += city[i]
-        continue
-    break
+with c2:
+    # search or get city from ip
+    st.write("#")
+    st.write("###")
+    st.write("###")
+    citySearchInput = st.text_input("Search for a city:")
+    if citySearchInput:
+        citySearchURL = "https://nominatim.openstreetmap.org/search?city=" + citySearchInput + "&format=json"
+        citySearchResponse = rq.get(citySearchURL).json()
+        if len(citySearchResponse) > 0:
+            cityOptions = []
+            for item in citySearchResponse:
+                if item["display_name"] not in cityOptions:
+                    cityOptions.append(item["display_name"])
+            # cityOptions = list(set(cityOptions))
+            noOfCityOptions = len(cityOptions)
+            if noOfCityOptions > 1:
+                selectedCity = st.selectbox(str(noOfCityOptions) + " results for \"" + citySearchInput + "\"", cityOptions)
+            else:
+                selectedCity = st.selectbox("1 result for \"" + citySearchInput + "\"", cityOptions)
+            selectedLat = 0.
+            selectedLon = 0.
+            for item in citySearchResponse:
+                if item["display_name"] == selectedCity:
+                    selectedLat = item["lat"]
+                    selectedLon = item["lon"]
+                    break
+            city = selectedCity
+            lat = selectedLat
+            lon = selectedLon
+        else:
+            st.error("Your search for \"" + citySearchInput + "\" returned 0 results. Try another city.")
+            # lat lon from ip
+            g = geocoder.ipinfo('me')
+            lat = g.latlng[0]
+            lon = g.latlng[1]
+            city = g.city
+            st.info("Based on your IP address, you are in or near " + city + ".")
+    else:
+        # lat lon from ip
+        g = geocoder.ipinfo('me')
+        lat = g.latlng[0]
+        lon = g.latlng[1]
+        city = g.city
+        st.info("Based on your IP address, you are in or near " + city + ".")
 
-map_checkbox = st.checkbox("Display Map of " + cityShortName)
-if map_checkbox:
-    map_creator(lat,lon)
+    # get response from openWeatherMap api
+    apiKey = "f2753510aec1cb9ef2f826c3edd452ad"
+    apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+str(lat)+"&lon="+str(lon)+"&appid=" + apiKey
+    response = rq.get(apiURL).json()
 
-st.header("Statistics for " + cityShortName)
+    #parse response
+    currentDateTime = dt.datetime.fromtimestamp(response["current"]["dt"])
+    sunriseToday = dt.datetime.fromtimestamp(response["current"]["sunrise"])
+    sunsetToday = dt.datetime.fromtimestamp(response["current"]["sunset"])
 
-# get response from openWeatherMap api
-apiKey = "f2753510aec1cb9ef2f826c3edd452ad"
-apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+str(lat)+"&lon="+str(lon)+"&appid=" + apiKey
-response = rq.get(apiURL).json()
+    currentTempKelvin = response["current"]["temp"]
+    currentFeelsLikeKelvin = response["current"]["feels_like"]
+    currentDewPointKelvin = response["current"]["dew_point"]
 
-#parse response
-currentDateTime = dt.datetime.fromtimestamp(response["current"]["dt"])
-sunriseToday = dt.datetime.fromtimestamp(response["current"]["sunrise"])
-sunsetToday = dt.datetime.fromtimestamp(response["current"]["sunset"])
+    currentPressure = response["current"]["pressure"]
+    currentPressureStr = str(currentPressure) + " hPa"
 
-currentTempKelvin = response["current"]["temp"]
-currentFeelsLikeKelvin = response["current"]["feels_like"]
-currentDewPointKelvin = response["current"]["dew_point"]
+    currentHumidity = response["current"]["humidity"]
+    currentClouds = response["current"]["clouds"]
+    currentHumidityStr = str(currentHumidity) + "%"
+    currentCloudsStr = str(currentClouds) + "%"
 
-currentPressure = response["current"]["pressure"]
-currentPressureStr = str(currentPressure) + " hPa"
+    currentUVI = response["current"]["uvi"]
+    currentUVIStr = str(currentUVI)
 
-currentHumidity = response["current"]["humidity"]
-currentClouds = response["current"]["clouds"]
-currentHumidityStr = str(currentHumidity) + "%"
-currentCloudsStr = str(currentClouds) + "%"
+    currentVisibilityM = response["current"]["visibility"]
+    currentWindSpeedMS = response["current"]["wind_speed"]
 
-currentUVI = response["current"]["uvi"]
-currentUVIStr = str(currentUVI)
+    currentWindDeg = response["current"]["wind_deg"]
+    currentWindDirStr = ""
+    if (currentWindDeg >= 337.5) | (currentWindDeg < 22.5):
+        currentWindDirStr = "N"
+    elif currentWindDeg < 67.5:
+        currentWindDirStr = "NE"
+    elif currentWindDeg < 112.5:
+        currentWindDirStr = "E"
+    elif currentWindDeg < 157.5:
+        currentWindDirStr = "SE"
+    elif currentWindDeg < 202.5:
+        currentWindDirStr = "S"
+    elif currentWindDeg < 247.5:
+        currentWindDirStr = "SW"
+    elif currentWindDeg < 292.5:
+        currentWindDirStr = "W"
+    else:
+        currentWindDirStr = "NW"
 
-currentVisibilityM = response["current"]["visibility"]
-currentWindSpeedMS = response["current"]["wind_speed"]
+    currentWeatherStr = response["current"]["weather"][0]["description"]
 
-currentWindDeg = response["current"]["wind_deg"]
-currentWindDirStr = ""
-if (currentWindDeg >= 337.5) | (currentWindDeg < 22.5):
-    currentWindDirStr = "N"
-elif currentWindDeg < 67.5:
-    currentWindDirStr = "NE"
-elif currentWindDeg < 112.5:
-    currentWindDirStr = "E"
-elif currentWindDeg < 157.5:
-    currentWindDirStr = "SE"
-elif currentWindDeg < 202.5:
-    currentWindDirStr = "S"
-elif currentWindDeg < 247.5:
-    currentWindDirStr = "SW"
-elif currentWindDeg < 292.5:
-    currentWindDirStr = "W"
-else:
-    currentWindDirStr = "NW"
+    # selectbox
+    temperatureUnit = st.selectbox("Select a unit of temperature", ["Fahrenheit", "Celsius"])
 
-currentWeatherStr = response["current"]["weather"][0]["description"]
+    # convert to selected unit and build strings
+    currentTemp = k2c(currentTempKelvin)
+    currentFeelsLike = k2c(currentFeelsLikeKelvin)
+    currentDewPoint = k2c(currentDewPointKelvin)
+    if temperatureUnit == "Fahrenheit":
+        currentTemp = c2f(currentTemp)
+        currentFeelsLike = c2f(currentFeelsLike)
+        currentDewPoint = c2f(currentDewPoint)
+    currentTempStr = str(int(currentTemp)) + "°" + temperatureUnit[0]
+    currentFeelsLikeStr = str(int(currentFeelsLike)) + "°" + temperatureUnit[0]
+    currentDewPointStr = str(int(currentDewPoint)) + "°" + temperatureUnit[0]
 
-temperatureUnit = st.selectbox("Select a unit of temperature", ["Fahrenheit", "Celsius"])
+    currentVisibility = currentVisibilityM
+    currentWindSpeed = currentWindSpeedMS
+    currentVisibilityStr = ""
+    currentWindSpeedStr = ""
+    if temperatureUnit == "Fahrenheit":
+        currentVisibility = m2mi(currentVisibility)
+        currentWindSpeed = ms2mph(currentWindSpeed)
+        currentVisibilityStr = str(int(currentVisibility)) + " miles"
+        currentWindSpeedStr = str(int(10*currentWindSpeed) / 10) + " mph"
+    else:
+        currentVisibilityStr = str(int(currentVisibility / 1000)) + " kilometers"
+        currentWindSpeedStr = str(int(10*currentWindSpeed) / 10) + " m/s"
 
-# convert to selected unit and build strings
-currentTemp = k2c(currentTempKelvin)
-currentFeelsLike = k2c(currentFeelsLikeKelvin)
-currentDewPoint = k2c(currentDewPointKelvin)
+    currentWindSpeedStr += " " + currentWindDirStr
 
-if temperatureUnit == "Fahrenheit":
-    currentTemp = c2f(currentTemp)
-    currentFeelsLike = c2f(currentFeelsLike)
-    currentDewPoint = c2f(currentDewPoint)
-
-currentTempStr = str(int(currentTemp)) + "°" + temperatureUnit[0]
-currentFeelsLikeStr = str(int(currentFeelsLike)) + "°" + temperatureUnit[0]
-currentDewPointStr = str(int(currentDewPoint)) + "°" + temperatureUnit[0]
-
-currentVisibility = currentVisibilityM
-currentWindSpeed = currentWindSpeedMS
-currentVisibilityStr = ""
-currentWindSpeedStr = ""
-
-if temperatureUnit == "Fahrenheit":
-    currentVisibility = m2mi(currentVisibility)
-    currentWindSpeed = ms2mph(currentWindSpeed)
-    currentVisibilityStr = str(int(currentVisibility)) + " miles"
-    currentWindSpeedStr = str(int(10*currentWindSpeed) / 10) + " mph"
-else:
-    currentVisibilityStr = str(int(currentVisibility / 1000)) + " kilometers"
-    currentWindSpeedStr = str(int(10*currentWindSpeed) / 10) + " m/s"
-
-currentWindSpeedStr += " " + currentWindDirStr
-# all info should be available now, the rest of the app is below
-
-# display all possible information
-
-data = [{'Current Time': str(currentDateTime), 'Sunrise': str(sunriseToday), 'Sunset': str(sunsetToday)}]
-st.dataframe(data)
-# st.write("Current Time: " + str(currentDateTime))
-# st.write("Sunrise Today: " + str(sunriseToday))
-# st.write("Sunset Today: " + str(sunsetToday))
-
-st.write("Current Temperature: " + currentTempStr)
-st.write("Feels Like: " + currentFeelsLikeStr)
-st.write("Dew Point: " + currentDewPointStr)
-
-st.write("Pressure: " + currentPressureStr)
-
-st.write("Clouds: " + currentCloudsStr)
-st.write("Humidity: " + currentHumidityStr)
-
-seeUVI = st.checkbox("See UV Index information.")
-if seeUVI:
-    c1, c2 = st.columns([2, 1])
-    with c1:
-        UVILabel = [""]
-        UVIWidth = 2
-        UVIYTicks = [0, 3, 6, 8, 11]
-
-        fig, ax = plt.subplots()
-
-        ax.bar(UVILabel, 10, UVIWidth, bottom=11, label='Extreme', color="#B94375")
-        ax.bar(UVILabel, 3, UVIWidth, bottom=8, label='Very High', color="#C73734")
-        ax.bar(UVILabel, 2, UVIWidth, bottom=6, label='High', color="#E09945")
-        ax.bar(UVILabel, 3, UVIWidth, bottom=3, label='Moderate', color="#F9DC4F")
-        ax.bar(UVILabel, 3, UVIWidth, bottom=0, label='Low', color="#8DBF89")
-        ax.plot(0.5, currentUVI, "k*", markersize=20)
-
-        ax.set_title("UV Index")
-        ax.set_yticks(UVIYTicks)
-        ax.set_xticks([])
-        ax.spines["top"].set_visible(False)
-        ax.spines["left"].set_visible(False)
-        ax.spines["right"].set_visible(False)
-        ax.spines["bottom"].set_visible(False)
-        plt.xlim(0, 1)
-        plt.ylim(0, 15)
-        ax.legend()
-        st.pyplot(fig)
-
+    seeUVI = st.checkbox("See UV Index information.")
+    if seeUVI:
         if currentUVI < 3:
             currentUVIStr = "low"
         elif currentUVI < 6:
@@ -224,17 +186,102 @@ if seeUVI:
             currentUVIStr = "very high"
         else:
             currentUVIStr = "extreme"
-
-    with c2:
-        st.write("###")
         st.info("The UV Index in this area is **" + currentUVIStr + "** (" + str(currentUVI) + ").")
-        link = '[Find out more about the UV Index.](https://www.epa.gov/sites/default/files/documents/uviguide.pdf)'
+        UVILabel = [""]
+        UVIWidth = 2
+        UVIYTicks = [0, 3, 6, 8, 11]
+
+        UVIfig, UVIax = plt.subplots()
+
+        UVIax.bar(UVILabel, 9.9, UVIWidth, bottom=11, label='Extreme', color="#B94375")
+        UVIax.bar(UVILabel, 2.9, UVIWidth, bottom=8, label='Very High', color="#C73734")
+        UVIax.bar(UVILabel, 1.9, UVIWidth, bottom=6, label='High', color="#E09945")
+        UVIax.bar(UVILabel, 2.9, UVIWidth, bottom=3, label='Moderate', color="#F9DC4F")
+        UVIax.bar(UVILabel, 2.9, UVIWidth, bottom=0, label='Low', color="#8DBF89")
+        UVIax.plot(0.5, currentUVI, "k*", markersize=20)
+
+        UVIax.set_title("UV Index", fontsize=20)
+        UVIax.set_yticks(UVIYTicks)
+        plt.yticks(fontsize=12)
+        UVIax.set_xticks([])
+        UVIax.spines["top"].set_visible(False)
+        UVIax.spines["left"].set_visible(False)
+        UVIax.spines["right"].set_visible(False)
+        UVIax.spines["bottom"].set_visible(False)
+        plt.xlim(0, 1)
+        plt.ylim(0, 15)
+        UVIax.legend(fontsize=12)
+        st.pyplot(UVIfig)
+
+        link = '[Find out more.](https://www.epa.gov/sites/default/files/documents/uviguide.pdf)'
         st.markdown(link, unsafe_allow_html=True)
 
-st.write("Wind Speed: " + currentWindSpeedStr)
-st.write("Visibility: " + currentVisibilityStr)
+    seeTime = st.checkbox("See Current Time, Sunrise, and Sunset")
 
-st.write("Weather Description: " + currentWeatherStr)
+    if seeTime:
+        data = [{'Current Time': str(currentDateTime), 'Sunrise': str(sunriseToday), 'Sunset': str(sunsetToday)}]
+        st.dataframe(data)
 
+# TODO: get hourly forecast from response
 
+# all info should be available now, the rest of the app is below
 
+# display all possible information
+
+with c1:
+    # header
+    cityShortName = ""
+    for i in range(len(city)):
+        if city[i] != ',':
+            cityShortName += city[i]
+            continue
+        break
+    st.title("Weather in " + cityShortName)
+    st.markdown("""
+        <style>
+            .big-font {
+                font-size:8rem !important;
+            }
+            .small-font {
+                font-size:3rem !important;
+            }
+            .my {
+            line-height:1.1;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+
+    icon = response["current"]["weather"][0]["icon"]
+    iconSource = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+    st.markdown("""
+        <div class='my' style='background:lightblue; padding:1rem; margin-bottom:1rem; border-radius:1rem'>
+            <span style='display:flex; align-items:center; margin:0; padding:0;'>
+                <p style='display: inline; margin:0; padding:0;' class='big-font'>""" + currentTempStr + """</p>
+                <img style='display: inline; margin:0; padding:0; width: 8rem; height: 8rem;' src='""" + iconSource + """' width='0' height='0'>
+            </span>
+            <p style='margin:0; padding:0;' class='small-font'><i>""" + currentWeatherStr + """</i></p>
+            <p style='margin:0; padding:0;' class='small-font'>Feels like """ + currentFeelsLikeStr + """</p>
+            <p style='margin:0; padding:0;' class='small-font'>Humidity: """ + currentHumidityStr + """</p>
+        </div>
+        """, unsafe_allow_html=True)
+
+    map_creator(lat,lon)
+
+    #st.write("Current Time: " + str(currentDateTime))
+    #st.write("Sunrise Today: " + str(sunriseToday))
+    #st.write("Sunset Today: " + str(sunsetToday))
+
+    #st.write("Current Temperature: " + currentTempStr)
+    #st.write("Feels Like: " + currentFeelsLikeStr)
+    #st.write("Dew Point: " + currentDewPointStr)
+
+    #st.write("Pressure: " + currentPressureStr)
+
+    #st.write("Clouds: " + currentCloudsStr)
+    #st.write("Humidity: " + currentHumidityStr)
+
+    #st.write("Wind Speed: " + currentWindSpeedStr)
+    #st.write("Visibility: " + currentVisibilityStr)
+
+    #st.write("Weather Description: " + currentWeatherStr)
